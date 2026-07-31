c7b0859bf3fd9a3dc756f0f91001079792571c28
imalzy <imalzaya19@gmail.com>
2026-07-31 17:57:03 +0700

    pertemuan 1

---
+++ Pertemuan 1/main.js
@@ -0,0 +1,23 @@
+// setiap kali, ingin menuliskan kode javascript,
+// harus membuat file dengan extentions .js
+
+
+// Penulisan JS
+//  titik koma di akhir, boleh juga tidak
+alert('I am coming from out side');
+alert('Halo, Saya ingin belajar JS');
+
+// KODE TIDAK AKAN DI EKSEKUSI OLEH PROGRAM
+// COMMENT
+// comment 1 line
+// alert('Saya comment');
+
+
+// comment 1 block
+/*
+ function sayHello(){
+        return "Halo, Selamat datang!!"
+    }
+ */
+
+
